54fbcaa869befb684eb2e0b6369fa513680f6dd0
daniel <g.thirteen333@gmail.com>
2019-11-13 02:12:24 -0500

    weather api successfully retrieves object in console

--- src/WeekContainer.js
+++ src/WeekContainer.js
@@ -1,8 +1,21 @@
 import React from 'react';
-import {apiKeys} from './apiKeys';
+import key from './apiKeys';
 class WeekContainer extends React.Component {
 
+    
+    componentDidMount(){
+        debugger
+        let masterKey = key;
+        debugger
+        const weatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=${masterKey.key}`
+        fetch(weatherUrl)
+            .then(res => res.json())
+            .then(data => console.log(data))
+    }
+
+
     render(){
+        
         return (
             <div>
                 <h1>Hello World</h1>
