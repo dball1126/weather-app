5738f5d65f0f4482a1d4f47e11996914a8e4b313
daniel <g.thirteen333@gmail.com>
2019-11-14 01:42:55 -0500

    all days display on the dom

---
+++ src/Day.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Day = ({data}) => {
+    
+
+        return (
+            <div>
+                {"day"}
+            </div>
+        )
+}
+
+export default Day;
--- src/WeekContainer.js
+++ src/WeekContainer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import key from './apiKeys';
+import Day from './Day';
 class WeekContainer extends React.Component {
     state = {
         fullData: [],
@@ -7,16 +8,16 @@ class WeekContainer extends React.Component {
     }
     
     componentDidMount(){
-        debugger
+        
         let masterKey = key;
-        debugger
+        
         const weatherUrl = `http://api.openweathermap.org/data/2.5/forecast?zip=11215,us&APPID=${masterKey.key}`
         fetch(weatherUrl)
             .then(res => res.json())
             .then(data => {
-                debugger
+                
                 const dailyData = data.list.filter(current => current.dt_txt.includes("18:00:00"))
-                debugger
+                
                 this.setState({
                     dailyData: dailyData,
                     fullData: data.list
@@ -24,12 +25,18 @@ class WeekContainer extends React.Component {
             })
     }
 
+    days(){
+        return this.state.dailyData.map((data, idx) => (
+            <Day key={idx} data={data}/>
+        ))
+    }
 
-    render(){
-        
+
+    render(){  
         return (
             <div>
                 <h1>Hello World</h1>
+                {this.days()}
             </div>
         )
     }
