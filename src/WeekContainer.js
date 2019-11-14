ebf9ad16a1a1df171356b6c7744f852c16d22600
daniel <g.thirteen333@gmail.com>
2019-11-13 22:41:08 -0500

    daily data and full data for the week parsed sucessfully

--- src/WeekContainer.js
+++ src/WeekContainer.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import key from './apiKeys';
 class WeekContainer extends React.Component {
-
+    state = {
+        fullData: [],
+        dailyData: []
+    }
     
     componentDidMount(){
         debugger
         let masterKey = key;
         debugger
-        const weatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=${masterKey.key}`
+        const weatherUrl = `http://api.openweathermap.org/data/2.5/forecast?zip=11215,us&APPID=${masterKey.key}`
         fetch(weatherUrl)
             .then(res => res.json())
-            .then(data => console.log(data))
+            .then(data => {
+                debugger
+                const dailyData = data.list.filter(current => current.dt_txt.includes("18:00:00"))
+                debugger
+                this.setState({
+                    dailyData: dailyData,
+                    fullData: data.list
+                }, () => console.log(this.state))
+            })
     }
 
 
